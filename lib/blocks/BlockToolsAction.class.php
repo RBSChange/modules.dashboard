f5e8ab6f9312f84e4b34a7d3e10f6c1a0803acac
Gaël PORT <gael.port@rbs.fr>
2010-02-12 15:35:35 +0000

    [FIX #23523] édition de locales

--- lib/blocks/BlockToolsAction.class.php
+++ lib/blocks/BlockToolsAction.class.php
@@ -10,17 +10,40 @@ class dashboard_BlockToolsAction extends website_BlockAction
 	 */
 	function execute($request, $response)
 	{		
-		if (users_UserService::getInstance()->getCurrentBackEndUser()->getIsroot())
+		$packages = array();
+		$canEditLocale = false;
+		$user = users_UserService::getInstance()->getCurrentBackEndUser();
+		
+		if ($user->getIsroot())
 		{
-			$packages = array('framework' => 'Framework');
+			$packages['framework'] = 'Framework';
 			foreach (ModuleService::getInstance()->getPackageVersionList() as $packageName => $version)
 			{
 				$moduleName = substr($packageName, 8);
 				$packages[$moduleName] = f_Locale::translate('&modules.' . $moduleName . '.bo.general.Module-name;');
 			}
-			$request->setAttribute('packages', $packages);
-			$request->setAttribute('canEditLocale', true);
+			$canEditLocale = true;
 		}
+		else 
+		{
+			$ps = f_permission_PermissionService::getInstance();
+			$ms = ModuleService::getInstance();
+			foreach (ModuleService::getInstance()->getPackageVersionList() as $packageName => $version)
+			{
+				$moduleName = substr($packageName, 8);
+				$prefixConstModulename = strtoupper('mod_'.$moduleName.'_');
+				$enabled = constant($prefixConstModulename . 'ENABLED');
+				$visible = constant($prefixConstModulename . 'VISIBLE');
+				if ($enabled && $visible && $ps->hasPermission($user, $packageName . '.EditLocale', $ms->getRootFolderId($moduleName)))
+				{
+					$packages[$moduleName] = f_Locale::translate('&modules.' . $moduleName . '.bo.general.Module-name;');
+					$canEditLocale = true;
+				}
+			}
+		}
+		
+		$request->setAttribute('packages', $packages);
+		$request->setAttribute('canEditLocale', $canEditLocale);
 		return website_BlockView::SUCCESS;
 	}
 }
