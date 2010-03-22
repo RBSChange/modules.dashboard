b676bb4126bffb9c5744f7f0d0e6c16af2c01014
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-03-22 15:22:23 +0000

    [NEW] Ajout du parametrage de la langue du BO depuis l'extension

--- actions/DashboardAction.class.php
+++ actions/DashboardAction.class.php
@@ -7,16 +7,20 @@ class dashboard_DashboardAction extends f_action_BaseAction
 	 */
 	public function _execute($context, $request)
 	{
+		$rc = RequestContext::getInstance();
+        $rc->setUILangFromParameter($request->getParameter('uilang'));
+        controller_ChangeController::setNoCache();
         try
         {
-        	controller_ChangeController::setNoCache();
+        	$rc->beginI18nWork($rc->getLang());
             $userDocument = users_UserService::getInstance()->getCurrentBackEndUser();
             $page = dashboard_DashboardService::getInstance()->getTemporaryPageFromUser($userDocument);
             website_PageService::getInstance()->render($page);
+            $rc->endI18nWork();
         }
         catch (Exception $e)
         {  
-        	Framework::exception($e);     
+        	$rc->endI18nWork($e);     
         }
 		return View::NONE;
 	}
