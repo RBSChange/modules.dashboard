a5617e1f04659f0e10781317d618c3c958300662
Eric HAUSWALD <eric.hauswald@rbs.fr>
2010-11-26 08:56:31 +0100

    [UPDATE] Mise à jour locales

--- lib/blocks/BlockToolsAction.class.php
+++ lib/blocks/BlockToolsAction.class.php
@@ -10,40 +10,7 @@ class dashboard_BlockToolsAction extends website_BlockAction
 	 */
 	function execute($request, $response)
 	{		
-		$packages = array();
-		$canEditLocale = false;
 		$user = users_UserService::getInstance()->getCurrentBackEndUser();
-		
-		if ($user->getIsroot())
-		{
-			$packages['framework'] = 'Framework';
-			foreach (ModuleService::getInstance()->getPackageVersionList() as $packageName => $version)
-			{
-				$moduleName = substr($packageName, 8);
-				$packages[$moduleName] = f_Locale::translate('&modules.' . $moduleName . '.bo.general.Module-name;');
-			}
-			$canEditLocale = true;
-		}
-		else 
-		{
-			$ps = f_permission_PermissionService::getInstance();
-			$ms = ModuleService::getInstance();
-			foreach (ModuleService::getInstance()->getPackageVersionList() as $packageName => $version)
-			{
-				$moduleName = substr($packageName, 8);
-				$prefixConstModulename = strtoupper('mod_'.$moduleName.'_');
-				$enabled = constant($prefixConstModulename . 'ENABLED');
-				$visible = constant($prefixConstModulename . 'VISIBLE');
-				if ($enabled && $visible && $ps->hasPermission($user, $packageName . '.EditLocale', $ms->getRootFolderId($moduleName)))
-				{
-					$packages[$moduleName] = f_Locale::translate('&modules.' . $moduleName . '.bo.general.Module-name;');
-					$canEditLocale = true;
-				}
-			}
-		}
-		
-		$request->setAttribute('packages', $packages);
-		$request->setAttribute('canEditLocale', $canEditLocale);
 		return website_BlockView::SUCCESS;
 	}
 }
