87ad31ab1c9d50158d4eed5afbb7dfe22e37ab06
Gaël PORT <gael.port@rbs.fr>
2011-05-04 09:38:01 +0200

    [FIX] Fixed dashboard blocks refresh.

--- actions/ViewBlockAction.class.php
+++ actions/ViewBlockAction.class.php
@@ -7,18 +7,21 @@ class dashboard_ViewBlockAction extends f_action_BaseAction
 	 */
 	public function _execute($context, $request)
 	{
-		$user = $this->getBackEndUser();	
-		if (!$user) {return View::NONE;}
+		$user = $this->getBackEndUser();
+		if (!$user)
+		{
+			return View::NONE;
+		}
 		$blocType = $request->getModuleParameter('dashboard', 'type');
 		$package = explode('_', $blocType);
 		$moduleName = $package[0];
-		
+
 		$displayParam = $request->getModuleParameter('dashboard', 'display');
-		if (!is_array($displayParam)) {$displayParam = array();}
-		
-		
-		$blockClassName = $moduleName . '_Block' . ucfirst($package[1]) . 'Action';
-		
+		if (!is_array($displayParam))
+		{
+			$displayParam = array();
+		}
+
 		try
 		{
 			$bt = 'modules_'. $moduleName . '_'.$package[1];
@@ -28,23 +31,23 @@ class dashboard_ViewBlockAction extends f_action_BaseAction
 				throw new Exception("Block $bt not found");
 			}
 			$blockClass = new ReflectionClass($blockClassName);
-			
+				
 			$moduleParams = $request->getParameter($moduleName.'Param', array());
 			$moduleParams['refresh'] = true;
 			$request->setParameter($moduleName.'Param', $moduleParams);
-			
+				
 			$blockController = website_BlockController::getInstance();
 			$page = dashboard_DashboardService::getInstance()->getTemporaryPageFromUser($user);
 			$blockController->setPage($page);
 			$blockController->getContext()->setAttribute(website_BlockAction::BLOCK_BO_MODE_ATTRIBUTE, false);
-			$blockInstance = $blockClass->newInstance($bt);			
+			$blockInstance = $blockClass->newInstance($bt);
 			foreach ($displayParam as $name => $value)
 			{
 				$blockInstance->setConfigurationParameter($name, $value);
-			}	
+			}
 			$blockController->process($blockInstance, f_mvc_HTTPRequest::getInstance());
 			$blockContent = $blockController->getResponse()->getWriter()->getContent();
-			
+				
 			$title = $blockInstance->getTitle();
 			$icon = $blockInstance->getIcon();
 			$openModule = $blockInstance->getOpenModule();
@@ -57,21 +60,21 @@ class dashboard_ViewBlockAction extends f_action_BaseAction
 			$icon = '';
 			$openModule = false;
 		}
-		
+
 		controller_ChangeController::setNoCache();
-		header('Content-Type' . ':' . 'text/xml');	
+		header('Content-Type' . ':' . 'text/xml');
 		$this->write($title, $icon, $blockContent, $openModule);
 		return View::NONE;
 	}
-	
+
 	/**
 	 * @return users_persistentdocument_backenduser
 	 */
 	protected function getBackEndUser()
 	{
-		 return users_UserService::getInstance()->getCurrentBackEndUser();
+		return users_UserService::getInstance()->getCurrentBackEndUser();
 	}
-	
+
 	/**
 	 * @param string $title
 	 * @param string $icon
@@ -88,19 +91,19 @@ class dashboard_ViewBlockAction extends f_action_BaseAction
 			$output->writeAttribute('hideGotoModule', 'true');
 		}
 		$output->writeElement('title', $title);
-		
-		$output->startElement('icon');
-		$output->writeAttribute('image', $icon);	
-		$output->endElement(); //icon
-		
-		$output->startElement('content');
-                $output->text($content);
-                $output->endElement(); //content
 
+		$output->startElement('icon');
+		$output->writeAttribute('image', $icon);
+		$output->endElement(); //icon
+
+		$output->startElement('content');
+		$output->startCdata();
+		$output->text($content);
+		$output->endCdata();
 		$output->endElement(); //content
-		
+
 		$output->endElement(); //dashboard-widget
 		$output->endDocument(); //DOCUMENT
-		echo $output->outputMemory(true);		
+		echo $output->outputMemory(true);
 	}
 }
